d5eb70250d826198f4d81583733ca1c208f0c725
Adam Whitlock <adam.whitlock627@gmail.com>
2019-05-11 15:40:27 -0600

    fancy hover effects

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,25 +1,47 @@
 <template>
   <on-click-outside :do="handleClickOutside">
     <div class="relative mx-auto">
-      <a @click="open = !open" type="button" class="w-full block bg-transparent cursor-pointer text-white rounded py-2 focus:outline-0">
-        {{dropdownText}}&nbsp; <font-awesome :icon="['fas', 'chevron-down']" :class="dropdownStatus"/>
+      <a
+        type="button"
+        class="w-full block bg-transparent cursor-pointer text-white rounded py-2 focus:outline-0"
+        @click="open = !open"
+      >
+        {{ dropdownText }}&nbsp; <font-awesome
+          :icon="['fas', 'chevron-down']"
+          :class="dropdownStatus"
+        />
       </a>
       <transition name="dropdownFade">
-      <div v-show="open" class="px-4 py-2 rounded bg-white border absolute z-50 w-32 shadow-lg">
-         <div v-for="(link, index) in links" :key="index" class="my-3 mx-2">
-        <g-link class="text-cyan-darker leading-normal no-underline hover:text-magenta" :to="link.to">{{link.text}}</g-link>
-         </div>
-      </div>
+        <div
+          v-show="open"
+          class="px-4 py-2 rounded bg-white border absolute z-50 w-32 shadow-lg"
+        >
+          <div
+            v-for="(link, index) in links"
+            :key="index"
+            class="my-3 mx-2"
+          >
+            <g-link
+              class="text-cyan-darker leading-normal no-underline hover:text-magenta dropdown-link"
+              :to="link.to"
+            >
+              {{ link.text }}
+            </g-link>
+          </div>
+        </div>
       </transition>
     </div>
   </on-click-outside>
 </template>
 
 <script>
-import OnClickOutside from "./OnClickOutside.vue";
+import OnClickOutside from "./OnClickOutside.vue"
 
 export default {
   name: "Dropdown",
+  components: {
+    OnClickOutside
+  },
   props: {
     links: {
       type: Array,
@@ -30,19 +52,9 @@ export default {
       default: "No dropdown text"
     }
   },
-  components: {
-    OnClickOutside
-  },
   data() {
     return {
       open: false
-    };
-  },
-  methods: {
-    handleClickOutside() {
-      if (this.open) {
-        this.open = false;
-      }
     }
   },
   computed: {
@@ -53,8 +65,15 @@ export default {
         return "open"
       }
     }
+  },
+  methods: {
+    handleClickOutside() {
+      if (this.open) {
+        this.open = false
+      }
+    }
   }
-};
+}
 </script>
 
 <style>
@@ -72,4 +91,40 @@ export default {
   opacity: 0;
   transform: translateY(-5px);
 }
+
+.dropdown-link {
+  position: relative;
+}
+
+.dropdown-link::before, .dropdown-link::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 2px;
+  background-color: #ea148c;
+  transform: scaleX(0);
+  transition: transform 0.5s ease;
+}
+
+.dropdown-link::before {
+  top: -2px;
+  transform-origin: center right;
+}
+
+.dropdown-link:hover::before {
+  transform-origin: center left;
+  transform: scaleX(1);
+}
+
+.dropdown-link::after {
+  bottom: -2px;
+  transform-origin: center left;
+}
+
+.dropdown-link:hover::after {
+  transform-origin: center right;
+  transform: scaleX(1);
+}
+
 </style>
