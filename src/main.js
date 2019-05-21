5aded78e7a9bb4756ab567662ddbe37c22c19249
Adam Whitlock <adam.whitlock627@gmail.com>
2019-05-20 19:28:06 -0600

    moved lightbox styles to default layout

--- src/main.js
+++ src/main.js
@@ -2,8 +2,4 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 export default function(Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
-  //  head.script.push({
-  //   src: 'https://cdnjs.com/libraries/baguettebox.js',
-  //   body: true
-  //  })
 }
