2cb7c58a9bffb525e430add008ac38dd3c6c4d59
Adam Whitlock <adam.whitlock627@gmail.com>
2019-06-02 20:49:16 -0600

    fix image sizing on betapic

--- src/pages/Betapic.vue
+++ src/pages/Betapic.vue
@@ -10,15 +10,21 @@
           />
         </div>
 
-        <div class="text-white text-center p-6 md:pr-16 bg-white rounded md:w-1/3">
+        <div
+          class="text-white text-center p-6 md:pr-16 bg-white rounded md:w-1/3"
+        >
           <h4 class="text-4xl text-magenta m-0 mb-3">
             Betapic: unified search for creative commons images.
           </h4>
-          <p class="font-extrabold mx-12 my-0 text-magenta-darker rounded px-10 py-4 bg-white shadow">
+          <p
+            class="font-extrabold mx-12 my-0 text-magenta-darker rounded px-10 py-4 bg-white shadow"
+          >
             tech used: node, express, various apis, ejs, jquery.
           </p>
           <p class="text-magenta mb-4">
-            Necessity is the mother of invention. As a designer, I wanted to simplify the search for creative commons image assets. Betapic brings Unsplash, Pixabay, and Pexels together into one interface.
+            Necessity is the mother of invention. As a designer, I wanted to
+            simplify the search for creative commons image assets. Betapic
+            brings Unsplash, Pixabay, and Pexels together into one interface.
           </p>
           <div class="mt-8 mb-8">
             <a
@@ -31,12 +37,8 @@
           </div>
         </div>
 
-        <div class="hidden md:flex md:w-2/3 px-2 md:pr-4">
-          <g-image
-            class="w-full"
-            src="@/assets/images/betapic-screens-white.jpg"
-            alt=""
-          />
+        <div class="hidden md:block md:w-2/3 px-2 md:pr-4">
+          <g-image src="@/assets/images/betapic-screens-white.jpg" alt="" />
         </div>
       </div>
     </Layout>
@@ -44,14 +46,12 @@
 </template>
 
 <script>
-  export default {
-    metaInfo: {
+export default {
+  metaInfo: {
     title: 'Betapic photo search'
   }
-
-  }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
