eca30532014b697347206cde063899eae37aba84
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2025-04-24 08:40:54 -0600

    na

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,26 +1,28 @@
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faGithub,
   faTwitter,
-  faDribbble,
-} from "@fortawesome/free-brands-svg-icons";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+  faDribbble
+} from '@fortawesome/free-brands-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faGithub, faTwitter, faChevronDown, faDribbble, faGithub);
+library.add(faGithub, faTwitter, faChevronDown, faDribbble, faGithub)
+
+// adding some custom directives and components
 
 export default function(Vue) {
-  Vue.component("font-awesome", FontAwesomeIcon);
-  Vue.directive("scrolly", {
+  Vue.component('font-awesome', FontAwesomeIcon)
+  Vue.directive('scrolly', {
     bind(el, binding, vnode) {
-      const onclick = (event) => {
-        event.stopPropagation();
-        console.log(binding.value.id);
+      const onclick = event => {
+        event.stopPropagation()
+        console.log(binding.value.id)
         document.querySelector(binding.value.id).scrollIntoView({
-          behavior: "smooth",
-        });
-      };
-      el.addEventListener("click", onclick);
-    },
-  });
+          behavior: 'smooth'
+        })
+      }
+      el.addEventListener('click', onclick)
+    }
+  })
 }
